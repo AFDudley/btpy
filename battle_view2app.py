8f92bc483047ff231f3c408bb9be2747276ded71
A. F. Dudley <a.frederick.dudley@gmail.com>
2010-06-07 02:31:55 -0400

    Added dmg creation. TODO: clean exit of .exe and .app

--- battle_view2app.py
+++ battle_view2app.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 import py2app
 import os, shutil
+import commands
 
 APP = ['views/battle_view.py']
 DATA_FILES = ['views/DroidSansMono.ttf','yaml']
@@ -17,3 +18,5 @@ setup(
 yeah = 'dist/battle_view.app/Contents/Resources/'
 os.makedirs(yeah + 'views')
 shutil.move(yeah + 'DroidSansMono.ttf', yeah +'views/DroidSansMono.ttf')
+print "Converting app to dmg, please wait."
+print commands.getstatusoutput('hdiutil create -imagekey zlib-level=9 -srcfolder dist/battle_view.app dist/battle_view.dmg')[1]
