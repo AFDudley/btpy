e7ef8f1c990ca87bdbba8aea4e50652b79a9bcab
A. F. Dudley <a.frederick.dudley@gmail.com>
2013-01-21 00:18:44 -0500

    Removed equanimity/keep_alive.py and start.sh. Updated equanimity/notes.txt.

--- equanimity/keep_alive.py
+++
@@ -1,5 +0,0 @@
-import subprocess, time
-#hackity hackity
-ip = subprocess.check_output(["which", "ipython"]).rstrip("\n")
-cmd = [ip, "equanimity/battle_server.py", '(0, 0)']
-while True: subprocess.call(cmd)
