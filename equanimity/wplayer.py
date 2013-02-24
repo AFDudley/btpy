1c68f31b3fa2b472efc6bfa81d2802e18af40741
A. F. Dudley <a.frederick.dudley@gmail.com>
2013-02-23 23:13:45 -0500

    moved wplayer into own file from world.py

---
+++ equanimity/wplayer.py
@@ -0,0 +1,17 @@
+import hashlib
+import persistent
+
+class wPlayer(persistent.Persistent):
+    """Object that contains player infomration."""
+    def __init__(self, username, password=None):
+        persistent.Persistent.__init__(self)
+        self.username = username
+        #this is not secure, need to figure that out sometime
+        if password != None:
+            self.password = hashlib.md5(password).hexdigest()
+        else:
+            self.password = None
+        self.Fields  = persistent.mapping.PersistentMapping()
+        self.cookie   = None
+        self.roads    = None
+        self.treaties = None
