86f337a6c9354a1179c87b66e1072545088f2c51
A. F. Dudley <a.frederick.dudley@gmail.com>
2013-03-06 16:09:03 -0500

    Moved Silo into its own file.

--- equanimity/wstone.py
+++ equanimity/wstone.py
@@ -4,3 +4,6 @@ from persistent import Persistent
 class Stone(Persistent, cStone):
     def __init__(self, comp=None):
         cStone.__init__(self, comp)
+    
+    def __repr__(self):
+        return str(self.comp)
