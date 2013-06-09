82914d7603ef953c96d44613f16514abc784483d
A. F. Dudley <a.frederick.dudley@gmail.com>
2013-06-09 00:33:26 -0400

    basic jsonrpc working.

--- pyramid/world_server.py
+++ pyramid/world_server.py
@@ -1,18 +1,24 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid_rpc.jsonrpc import jsonrpc_method
 from datetime import datetime
-
 from equanimity.zeo import Zeo
-
 from copy import deepcopy
 
 class AuthHandler(object):
     def __init__(self, world):
         self.world = world
-        
-    def view(self, request, action):
-        return Response(str(self.world['Players']['World'].username))
+    #@jsonrpc_method(endpoint='signup')
+    def signup(self, request, params):
+        """Signup via json-rpc post"""
+        #{"jsonrpc": "2.0", "method": "signup", "params": [tom], "id": 1}
+        return Response(params)
+    def login(self): pass
+    def logout(self): pass
+    def view(self, request):
+        action = request.matchdict['action'].lower()
+        return Response(action)
         
 class WorldHandler(object):
     def __init__(self, world):
@@ -43,7 +49,7 @@ class PlayerHandler(object):
     def __init__(self, world):
         self.world = world
     def view(self, request, params):
-        stuff = params.matchdict['stuff'] 
+        stuff = params.matchdict['stuff']
         name = None
         attrib = None
         if len(stuff) > 0:
@@ -66,12 +72,16 @@ class PlayerHandler(object):
             return Response(str(stuff.matchdict))
 
 if __name__ == '__main__':
+
     zeo = Zeo()
     wr = zeo.root
     world = WorldHandler(wr)
     auth = AuthHandler(wr)
     players = PlayerHandler(wr)
     config = Configurator()
+    config.include('pyramid_rpc.jsonrpc')
+    config.add_jsonrpc_endpoint('signup', '/auth/signup')
+    config.add_jsonrpc_method(auth.signup, endpoint='signup', method='signup')
     #We don't need no sinking decorators.
     fieldhandlers = {}
     for x in xrange(wr['x']):
@@ -86,9 +96,9 @@ if __name__ == '__main__':
     
     config.add_route('world', '/world')
     config.add_view(world.view, route_name='world')
-    config.add_route('auth', '/auth/{action}')
-    config.add_view(auth.view, route_name='auth')
-    config.add_route('players', '/players/*action')
+    #config.add_route('auth', '/auth/{action}')
+    #config.add_view(auth.view, route_name='auth')
+    config.add_route('players', '/players/*stuff')
     config.add_view(players.view, route_name='players')
     app = config.make_wsgi_app()
     
